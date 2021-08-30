fd64962b036ddef5748afe0dd1cb9b610f47bced
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-08-30 00:14:55 -0700

    Initial commit

--- Routes/api/users.js
+++ Routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -31,7 +34,7 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(500).json({ error: [{ msg: 'User already exist' }] });
+        return res.status(400).json({ error: [{ msg: 'User already exist' }] });
       }
 
       const avatar = gravatar.url(email, {
@@ -47,11 +50,27 @@ router.post(
         avatar,
       });
 
-      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
 
-      // Rturn jsonwebtoken
+      user.password = await bcrypt.hash(password, salt);
 
-      res.send('Users route');
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
