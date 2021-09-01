0f7645fdd9327b2f4481eedca36913889bb14d89
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-08-31 23:28:13 -0700

    Initial commit

--- Routes/api/profile.js
+++ Routes/api/profile.js
@@ -109,4 +109,17 @@ router.post(
   }
 );
 
+//@route GET api/profile
+//@desc Get all/profile
+//@access Public
+router.get('/', async (req, res) => {
+  try {
+    let profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Sever Error');
+  }
+});
+
 module.exports = router;
