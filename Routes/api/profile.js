8bd1f76d594265e76669061ee8863b1f9da462dc
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-09-01 22:34:06 -0700

    Initial commit

--- Routes/api/profile.js
+++ Routes/api/profile.js
@@ -114,11 +114,31 @@ router.post(
 //@access Public
 router.get('/', async (req, res) => {
   try {
-    let profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
     res.json(profiles);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Sever Error');
+  }
+});
+
+//@route GET api/profile/user/:user_id
+//@desc Get profile by user ID
+//@access Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile)
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
 });
 
