2073791107668f0d788b3aa9b22c1b0039b3170f
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-09-04 01:43:45 -0700

    Initial commit

--- Routes/api/posts.js
+++ Routes/api/posts.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-//@route GET api/posts
-//@desc Test route
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+//@route POST api/posts
+//@desc Create a post
 //@access public
 router.get('/', (req, res) => {
   res.send('Posts route');
