f024042903a19e8fee6f67cece27e79f394fdb31
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-09-21 00:43:06 -0700

    Initial commit

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const DashboardAction = () => {
+const DashboardActions = () => {
   return (
     <div className='dash-buttons'>
       <Link to='/edit-profile' className='btn btn-light'>
@@ -17,4 +17,4 @@ const DashboardAction = () => {
   );
 };
 
-export default DashboardAction;
+export default DashboardActions;
