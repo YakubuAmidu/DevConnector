71259d3c5a422fdb1a260e2ac94d1551ffed6927
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-09-24 03:46:48 -0700

    Initial commit

---
+++ client/src/components/Profiles/ProfileItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileItem = () => async (dispatch) => {
+  return (
+    <div>
+      <h1>ProfileItem</h1>
+    </div>
+  );
+};
+
+export default ProfileItem;
---
+++ client/src/components/Profiles/Profiles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Profiles = () => async (dispatch) => {
+  return (
+    <div>
+      <h1>Profiles</h1>
+    </div>
+  );
+};
+
+export default Profiles;
