9150c62be6695de59cb66528a61d9a47eccb2156
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-09-24 04:20:54 -0700

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from './components/profile-form/CreateProfile';
 import EditProfile from './components/profile-form/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import AddEducation from './components/profile-form/AddEducation';
+import Profiles from './components/Profiles/Profiles';
 import AddExperience from './components/profile-form/AddExperience';
 
 //Redux
@@ -40,6 +41,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
                 exact
