314ade4a79778eaeb94c3dce6cac24412d4c515c
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-09-29 09:48:13 -0700

    Initial commit

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,4 +1,3 @@
-import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
@@ -15,10 +14,10 @@ const PostItem = ({
   return (
     <div class='post bg-white p-1 my-1'>
       <div>
-        <a href='profile.html'>
+        <Link to={`/profile/${user}`}>
           <img class='round-img' src={avatar} alt='' />
           <h4>{name}</h4>
-        </a>
+        </Link>
       </div>
       <div>
         <p class='my-1'>{text}</p>
