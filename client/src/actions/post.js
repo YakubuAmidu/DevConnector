e6b046db30edff72f05825c0ac82cea6432e02f3
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-09-28 23:21:40 -0700

    Initial commit

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
 
 // Get Post
 export const getPosts = () => async (dispatch) => {
@@ -39,7 +39,7 @@ export const addLike = (id) => async (dispatch) => {
 //Remove like
 export const removeLike = (id) => async (dispatch) => {
   try {
-    const res = await axios.put(`/api/posts/unlike${id}`);
+    const res = await axios.put(`/api/posts/unlike/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
@@ -48,6 +48,25 @@ export const removeLike = (id) => async (dispatch) => {
   } catch (err) {
     dispatch({
       type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//Delete post
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+
+    dispatch(setAlert('Post removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: DELETE_POST,
       payload: { msg: err.response.statusText, status: err.response.status },
     });
   }
