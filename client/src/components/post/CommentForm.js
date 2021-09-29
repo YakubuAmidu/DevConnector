d2aa282bcf0403cd8a627d87e98a15873e8998d9
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-09-29 10:53:58 -0700

    Initial commit

---
+++ client/src/components/post/CommentForm.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addComment } from '../../actions/post';
+
+const CommentForm = ({ addComment }) => {
+  return (
+    <div>
+      <h1>Comment Form</h1>
+    </div>
+  );
+};
+
+CommentForm.propTypes = {
+  addComment: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addComment })(CommentForm);
