0150720ad854ca4312bbab71fb41d57f2a80745c
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-09-30 00:44:00 -0700

    Prepare for deployment

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const router = require('../Routes/api/profile');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    });
+
+    console.log('mongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    //Exit process with failure
+    process.exit();
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
-
-const PORT = process.env.PORT || 5000;
 
 //Connect to database
 connectDB();
@@ -11,14 +10,22 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', function (req, res) {
-  res.send('API Running');
-});
-
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
